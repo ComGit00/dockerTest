be05f762538e247c4ae119ab453a2c26477a50f7
ComGit00 <an0160@daum.net>
2022-03-24 14:46:50 +0900

    확인용 페이지 수정

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import TestView from "../views/TestView.vue";
+import AboutView from "../views/AboutView.vue";
 
 Vue.use(VueRouter);
 
@@ -12,18 +12,9 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/testView",
-    name: "test",
-    component: TestView,
-  },
-  {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: AboutView,
   },
 ];
 
