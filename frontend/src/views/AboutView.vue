ee8e6ad641d1a0683b7382c5637c1f98230ebb0b
ComGit00 <an0160@daum.net>
2022-03-24 16:10:12 +0900

    about페이지 테스트용 수정

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <v-btn v-on:click="test1()">test1</v-btn><br />
+    <v-btn v-on:click="test2()">test2</v-btn><br />
+    <v-btn v-on:click="test3()">test2</v-btn><br />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import axios from "axios";
+
+export default {
+  methods: {
+    test1() {
+      axios.get("http://localhost:8794/test").then(function (res) {
+        console.log(res);
+      });
+    },
+    test2() {
+      axios.get("http://localhost/test").then(function (res) {
+        console.log(res);
+      });
+    },
+    test3() {
+      axios.get("http://localhost:8080/test").then(function (res) {
+        console.log(res);
+      });
+    },
+  },
+};
+</script>
